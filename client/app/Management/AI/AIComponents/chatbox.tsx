904c29bb83a40bfdfecd60e2bdb3b12722ee91e9
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-01-04 15:56:35 +0800

    Updating

--- client/app/Management/AI/AIComponents/chatbox.tsx
+++ client/app/Management/AI/AIComponents/chatbox.tsx
@@ -1,5 +1,7 @@
 "use client";
+import {useState} from "react";
 import { Mic, Plus, Hammer, ChevronDown } from "lucide-react";
+import { Question } from "../types";
 
 export default function ChatBox() {
     return (
--- client/app/Management/AI/types.ts
+++ client/app/Management/AI/types.ts
@@ -1,22 +1,6 @@
-interface ChatInputProps {
-    value: string;
-    onChange: (val: string) => void;
-    onSend: () => void;
-    disabled: boolean;
+export interface Question{
+    id: string;
+    text: string;
+    date_asked: Date;
+    answer?: string;
 }
-    export interface ChatResponse {
-    response: string;
-    status: string;
-}
-
-export interface PerformanceReport {
-    status: string;
-    report: {
-        total_posts: number;
-        total_likes: number;
-        total_views: number;
-        avg_engagement: number;
-        top_performing_post: string;
-    };
-}
-export type MessageRole = "user" | "ai" | "error";
