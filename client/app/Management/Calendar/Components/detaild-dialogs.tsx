5a0fade8dcf04afecea4ed5e640d8585d5d015b1
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-01-11 14:43:22 +0800

    Updating the Calendar Section

---
+++ client/app/Management/Calendar/Components/detaild-dialogs.tsx
@@ -0,0 +1,13 @@
+
+
+export default function DetailDDialogs() {
+    return (
+        <>
+            <main>
+                <div className="p-4 text-center bg-amber-900 text-white rounded-lg">
+                    Detail D Dialogs
+                </div>
+            </main>
+        </>
+    );
+}
