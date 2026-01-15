4fb376bd83c05fd187d62b98920fc91d55f99910
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-01-15 14:40:01 +0800

    Updating: Fixed the eror Typing

--- client/app/Management/Calendar/Calendar.tsx
+++ client/app/Management/Calendar/Calendar.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useState } from 'react';
 import { Calendar, dateFnsLocalizer, SlotInfo } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
-import enUS from 'date-fns/locale/en-US';
+import {format} from 'date-fns/format';
+import {parse} from 'date-fns/parse';
+import {startOfWeek} from 'date-fns/startOfWeek';
+import {getDay} from 'date-fns/getDay';
+import {enUS} from 'date-fns/locale/en-US';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import CustomEvent from './CustomEvent';
 import CustomToolbar from './CustomToolbar';
