1330627da93ef3a600e3a282f411739929a53fa7
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-01-18 12:47:27 +0800

    Update: Making Connect Social Media Modal to all platform

--- client/Components/ConnectSocial.tsx
+++ client/Components/ConnectSocial.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { useState } from "react";
 
+interface connectPlatformProps {
+    // Define any props if needed in the future
+    isOpen: boolean;
+    onClose: () => void;
+}
 
 export default function ConnectSocial() {
+
     const [isConnecting, setIsConnecting] = useState(false);
     return (
         <>
             <main>
-                <h1>Connect Social Component</h1>
+                <div className="bg-white p-6 rounded-2xl items-center justify-center shadow-lg shadow-black/5 mb-4">
+                    <h1>Connect Social Component</h1>
+                </div>
+                
             </main>
         </>
     );
